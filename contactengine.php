a6805baf43260ed25dd6a4a45b0181ff5f9f8d13
jroberts3363 <jroberts3363@users.noreply.github.com>
2019-04-06 13:53:18 -0400

    second attempt

---
+++ contactengine.php
@@ -0,0 +1,43 @@
+<?php
+
+//$EmailFrom = "#"; In case of spam problems we could use this.
+$EmailTo = "jesse.cne.roberts@gmail.com";
+$Subject = "Contact Form Submission";
+$Name = Trim(stripslashes($_POST['Name']));
+$Tel = Trim(stripslashes($_POST['Tel']));
+$Email = Trim(stripslashes($_POST['Email']));
+$Message = Trim(stripslashes($_POST['Message']));
+
+// validation
+$validationOK=true;
+if (!$validationOK) {
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.html\">";
+  exit;
+}
+
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $Name;
+$Body .= "\n";
+$Body .= "Tel: ";
+$Body .= $Tel;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $Email;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $Message;
+$Body .= "\n";
+
+// send email
+$success = mail($EmailTo, $Subject, $Body, "From: <$Email>");
+
+// redirect to success page
+if ($success){
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=contactthanks.php\">";
+}
+else{
+  print "<meta http-equiv=\"refresh\" content=\"0;URL=error.htm;\">";
+}
+?>
---
+++ contactthanks.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<head>
+	<title>Simple Contact Form</title>
+	<link rel="stylesheet" type="text/css" href="styles.css" />
+</head>
+
+<body>
+
+
+		<h1>Your message has been sent!</h1><br />
+		<p>Thank you for your email. I'll get back to you shortly.</p> <br />
+		<p>Jesse</p>
+
+
+</body>
+</html>
